d748208111afc9aa49a8bea4b0738fb46ce4d1b9
Kristy Currier <kristy.currier@gmail.com>
2019-01-28 11:06:52 -0500

    User registration page done, info passed to InfoSaved

---
+++ CoffeeShop/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using CoffeeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var user = new RegisterModel();
+            return View(user);
+        }
+
+        public ActionResult InfoSaved()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InfoSaved(RegisterModel user)
+        { 
+            return View(user);
+        }
+    }
+}
--- CoffeeShop/Models/RegisterModel.cs
+++ CoffeeShop/Models/RegisterModel.cs
@@ -17,7 +17,7 @@ namespace CoffeeShop.Models
         [Required]
         [MaxLength(50, ErrorMessage = "Please limit your last name to 50 characters")]
         [DisplayName("Last name")]
-        public string Description { get; set; }
+        public string LastName { get; set; }
 
         [Required(ErrorMessage = "Please select one!")]
         public Gender UserGender { get; set; }
@@ -31,12 +31,6 @@ namespace CoffeeShop.Models
         [RegularExpression(@"^\(?([2-9][0-8][0-9])\)?[-.●]?([2-9][0-9]{2})[-.●]?([0-9]{4})$", ErrorMessage ="Please enter a valid email in the form xxx-xxx-xxxx")]
         [DisplayName("Phone number")]
         public string PhoneNumber { get; set; }        
-
-        [Required]
-        [RegularExpression(@"^\d+.\d{0,2}$", ErrorMessage = "Please enter price with two decimal places and no dollar sign")]
-        [Range(0, 9999999999999999.99, ErrorMessage = "Please enter a valid price with only two decimal places")]
-        public decimal Price { get; set; }
-
     }
         public enum Gender
         {
